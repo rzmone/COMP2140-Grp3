d6315861a70c8f53ef3c02e9d922abd6c2fa4cd0
keanuty <keanutythompson@gmail.com>
2025-12-02 04:41:06 -0500

    Merge local SIMS project with remote README/.gitignore

---
+++ src/main/java/com/groupthree/sims/App.java
@@ -0,0 +1,13 @@
+package com.groupthree.sims;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
