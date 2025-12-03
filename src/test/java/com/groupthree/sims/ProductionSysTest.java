3ae4c6bc290cd30cbf298b8c0a55f47788e351a1
Ramone <1ramonewtsn@gmail.com>
2025-12-03 14:31:36 -0500

    Added placeholder for ProductionSysTest

---
+++ src/test/java/com/groupthree/sims/ProductionSysTest.java
@@ -0,0 +1,11 @@
+package com.groupthree.sims;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductionSysTest {
+
+    @Test
+    void placeholder() {
+        // does nothing, still passes
+    }
+}
